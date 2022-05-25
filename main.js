6b8c34f233b0feb5a1fbf43ad3e04f942787ff5b
godfrey <godiarksel@gmail.com>
2022-05-25 17:55:14 +0300

    Create and Add functionality to mobile menu and desktop links

---
+++ main.js
@@ -0,0 +1,19 @@
+const openMenu = document.querySelector('.open-menu');
+const menu = document.querySelector('.navigation__mobile');
+const closeMenu = document.querySelector('.close-menu');
+const interLinks = document.querySelectorAll('.internalLinks');
+
+function openLinks (links){
+  window.location.href = links;
+  menu.style.display = 'none';
+}
+
+openMenu.addEventListener('click',()=> {
+menu.style.display = 'block';
+});
+
+interLinks.addEventListener('click',openLinks);
+
+closeMenu.addEventListener('click',()=> {
+  menu.classList.toggle('closed');
+});
