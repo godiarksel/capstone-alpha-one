45561c6f4474d46e87254d731072eee32b59c98a
godfrey <godiarksel@gmail.com>
2022-05-26 05:53:40 +0300

    Run Linter tests

--- main.js
+++ main.js
@@ -3,21 +3,17 @@ const menu = document.querySelector('.navigation__mobile');
 const closeMenu = document.querySelector('.close-menu');
 const interLinks = document.querySelectorAll('.internalLinks');
 
-
-function openLinks (links){
+function openLinks(links) {
   window.location.href = links;
   menu.style.display = 'none';
 }
 
-openMenu.addEventListener('click',()=> {
-menu.style.display = 'block';
+openMenu.addEventListener('click', () => {
+  menu.style.display = 'block';
 });
 
-interLinks.addEventListener('click',openLinks);
+interLinks.addEventListener('click', openLinks);
 
-closeMenu.addEventListener('click',()=> {
+closeMenu.addEventListener('click', () => {
   menu.classList.toggle('closed');
 });
-
-
-
--- speakers.js
+++ speakers.js
@@ -1,42 +1,42 @@
 const speakerContainer = document.querySelector('.speakers__container');
 const speakerInfo = [
   {
-    img:'./images/socrates.png',
-    name:'Socrates',
-    title:'Greek Philosopher',
-    description:`Socrates was a Greek philosopher from Athens 
+    img: './images/socrates.png',
+    name: 'Socrates',
+    title: 'Greek Philosopher',
+    description: `Socrates was a Greek philosopher from Athens 
                  who is credited as the founder of Western philosophy 
                  and among the first moral philosophers of the ethical tradition of thought.`,
   },
   {
-    img:'./images/aristotle.png',
-    name:'Aristotle',
-    title:'Greek philosopher',
-    description:`Aristotle was a Greek philosopher and polymath during the Classical period in Ancient Greece. 
+    img: './images/aristotle.png',
+    name: 'Aristotle',
+    title: 'Greek philosopher',
+    description: `Aristotle was a Greek philosopher and polymath during the Classical period in Ancient Greece. 
                  Taught by Plato, he was the founder of the Lyceum, the Peripatetic school of philosophy, 
                  and the Aristotelian tradition.`,
   },
   {
-    img:'./images/marcus-aurelius.png',
-    name:'Marcus Aurelius',
-    title:'Roman Emperor',
-    description:`Marcus Aurelius Antoninus was Roman emperor from 161 to 180 and a Stoic philosopher. 
+    img: './images/marcus-aurelius.png',
+    name: 'Marcus Aurelius',
+    title: 'Roman Emperor',
+    description: `Marcus Aurelius Antoninus was Roman emperor from 161 to 180 and a Stoic philosopher. 
                  He was the last of the rulers known as the Five Good Emperors, and the last emperor of the Pax Romana, 
                  an age of relative peace and stability for the Roman Empire lasting from 27 BC to 180 AD.`,
   },
   {
-    img:'./images/seneca.png',
-    name:'Seneca',
-    title:'Roman Philosopher',
-    description:`Lucius Annaeus Seneca the Younger, usually known as Seneca, was a Roman Stoic philosopher, 
+    img: './images/seneca.png',
+    name: 'Seneca',
+    title: 'Roman Philosopher',
+    description: `Lucius Annaeus Seneca the Younger, usually known as Seneca, was a Roman Stoic philosopher, 
                  statesman, dramatist, and, in one work, satirist, from the post-Augustan age of Latin literature. 
                  Seneca was born in Cordoba in Hispania, and raised in Rome, where he was trained in rhetoric and philosophy.`,
   },
   {
-    img:'./images/plato.png',
-    name:'Plato',
-    title:'Greek Philosopher',
-    description:`Plato was a Greek philosopher born in Athens during the Classical period in Ancient Greece. 
+    img: './images/plato.png',
+    name: 'Plato',
+    title: 'Greek Philosopher',
+    description: `Plato was a Greek philosopher born in Athens during the Classical period in Ancient Greece. 
                  He founded the Platonist school of thought and the Academy, 
                  the first institution of higher learning in the Western world.`,
   },
